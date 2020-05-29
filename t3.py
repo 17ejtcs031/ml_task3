53e159a5d69a956a284efbdbfba0f18d0db31616
Himani Agarwal <52791723+17ejtcs031@users.noreply.github.com>
2020-05-30 00:28:41 +0530

    new-day

--- t3.py
+++ t3.py
@@ -85,7 +85,7 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        'cnn_dataset/training_set/',
+        '/root/ws/cnn_dataset/training_set/',
         target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
